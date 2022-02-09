f14a08523ff31f777823fc0ba94d728413b7bfd8
Sambaiah Jillellamudi <samba@MacBook-Air-de-Sambaiah.local>
2022-02-09 12:56:11 +0400

    new class added in String package

---
+++ src/com/learning/corejava/strings/GitRepoTest.java
@@ -0,0 +1,12 @@
+package com.learning.corejava.strings;
+
+public class GitRepoTest {
+
+	public static void main(String[] args) {
+		String name = "kavitha";
+		System.out.println("Name of the person is:" + name);
+		System.out.println(name + " logged into Git Repository.....");
+
+	}
+
+}
