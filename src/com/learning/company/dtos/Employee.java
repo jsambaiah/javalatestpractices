f3451c1380ed39a5abc937f5a997e0e44674f624
jsambaiah <jsambasivarao@gmail.com>
2022-02-09 13:18:32 +0400

    Update Employee.java

--- src/com/learning/company/dtos/Employee.java
+++ src/com/learning/company/dtos/Employee.java
@@ -68,7 +68,7 @@ public class Employee {
 
 	@Override
 	public String toString() {
-		return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + "]";
+		return "Employee Info [id=" + id + ", name=" + name + ", salary=" + salary + "]";
 	}
 
 	/**
